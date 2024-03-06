47f23a17c3a07d130e1aa7e74b5033bc363471d8
mukhriddinuz <109203312+Muhriddinuz@users.noreply.github.com>
2024-03-06 15:36:39 +0500

    education

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from .views import sing_up_user, signin_view, logout_user, edit_user, delete_user, user_profile
 
 urlpatterns = [
-
+    path('sing-up-user/', sing_up_user),
+    path('login-user/', signin_view),
+    path('log-out-user/', logout_user),
+    path('edit-user/<int:pk>/', edit_user),
+    path('delete-user/<int:pk>/', delete_user),
+    path('user-profile/<int:pk>/', user_profile),
 ]
